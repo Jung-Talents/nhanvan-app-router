b78ac33f74e1b3927114dca2242f7f8ad8a62fba
nhi.tny <id.tnyennhi@gmail.com>
2026-01-26 22:03:40 +1100

    Add CardCTA component

---
+++ app/components/index.ts
@@ -0,0 +1,4 @@
+// Export all components here
+export { default as CardCTA } from "./CardCTA";
+export { default as CardBenefits } from "./CardBenefits/CardBenefits";
+export { default as CardBenefitsList } from "./CardBenefits/CardBenefitsList";
