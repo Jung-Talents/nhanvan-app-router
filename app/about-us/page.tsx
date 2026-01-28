f73cade5fb3c2e930d113098d6b5a8a028df820d
wendyUVo <s3461279@student.rmit.edu.au>
2026-01-28 20:34:14 +1100

    Add Page About Us

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import CardIntroduction from "@/src/components/CardIntroduction/CardIntroduction";
+
+export default function AboutUsPage() {
+  return (
+    <main className="w-full bg-white">
+      <CardIntroduction />
+    </main>
+  );
+}
